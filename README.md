a085feafea4ecf67a4bbc7891c3d11d8d5c2198d
Alina Tertyshnikova <macintosh@MacBook-Pro-Macintosh-5.local>
2020-05-29 13:18:52 +0300

    hw_mobile_2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # AlinaTertyshnikovaMobile
+To launch web tests: mvn clean test -Pweb 
+
+To launch native tests: mvn clean test -Pnative
+
+You need opencv on the device to run nativeWithAutoCompleteTextViewTest 
---
+++ src/main/java/pageObjects/NativePageObject.java
@@ -0,0 +1,44 @@
+package pageObjects;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class NativePageObject {
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
+    WebElement signInBtn;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
+    WebElement registerBtn;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_email")
+    WebElement email;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_username")
+    WebElement username;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_password")
+    WebElement password;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_confirm_password")
+    WebElement confirmPassword;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_new_account_button")
+    WebElement registerAccount;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_email")
+    WebElement loginEmail;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_pwd")
+    WebElement loginPassword;
+
+    @AndroidFindBy(xpath = "//*[contains(@text, 'BudgetActivity')]")
+    WebElement budgetActivityTitle;
+
+    public NativePageObject(AppiumDriver appiumDriver) {
+        PageFactory.initElements(new AppiumFieldDecorator(appiumDriver), this);
+    }
+}
