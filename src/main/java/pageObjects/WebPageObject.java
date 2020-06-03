8d72bb5308990a63e1c8c8cb9f36c18d6c123ac0
Alina Tertyshnikova <macintosh@MacBook-Pro-Macintosh-5.local>
2020-06-03 20:14:55 +0300

    hw_mobile_2 fixes

--- src/main/java/pageObjects/WebPageObject.java
+++ src/main/java/pageObjects/WebPageObject.java
@@ -1,9 +1,19 @@
 package pageObjects;
 
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class WebPageObject {
+    @FindBy(xpath = "//input[@name='q']")
+    WebElement searchField;
+
+    @FindBy(xpath = "//div[@class='mnr-c xpd O9g5cc uUPGi']")
+    List<WebElement> results;
+
     public WebPageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements(appiumDriver, this);
     }
--- src/test/java/scenarios/webMobileTests.java
+++ src/test/java/scenarios/webMobileTests.java
@@ -1,6 +1,5 @@
 package scenarios;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -14,7 +13,7 @@ import static org.testng.Assert.assertFalse;
 public class webMobileTests extends BaseTest {
 
     @Test(groups = {"web"}, description = "Check that there are search query results in Google")
-    public void simpleWebTest() {
+    public void simpleWebTest() throws IllegalAccessException, NoSuchFieldException, InstantiationException {
         getDriver().get(ResourceManager.getProperty("url")); // open Google homepage
 
         // Make sure that page has been loaded completely
@@ -22,12 +21,12 @@ public class webMobileTests extends BaseTest {
                 wd -> ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete")
         );
 
-        WebElement searchField = getDriver().findElement(By.xpath("//input[@name='q']"));
+        WebElement searchField = getPo().getWelement("searchField");
         searchField.sendKeys(ResourceManager.getProperty("query"));
         searchField.sendKeys(Keys.ENTER);
 
         assertFalse(
-                getDriver().findElements(By.xpath("//div[@class='mnr-c xpd O9g5cc uUPGi']")).isEmpty(),
+                getPo().getWelements("results").isEmpty(),
                 "No founded results"
         );
 
